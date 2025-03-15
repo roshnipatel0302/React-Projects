b7a5722beffbd005dd1391dc1db215d0b799ef40
Roshni <roshnipatelrp2002@gmail.com>
2025-03-15 14:30:41 +0530

    Add AuthLayout, RTE, and PostForm components for enhanced user authentication and content creation

---
+++ Mega-Blog/src/components/pos-form/PostForm.jsx
@@ -0,0 +1,56 @@
+import React,{useCallback} from 'react'
+import { useForm } from 'react-hook-form'
+import {Button,Input,Select,RTE,} from './u'
+import appwriteService from '../../appwrite/config'
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+AuthService
+
+const PostForm = ({post}) => {
+    const {register,handleSubmit,watch,setValue,control,getValues}=useForm({
+        defaultValues: {
+            title: post?.title || "",
+            slug: post?.$id || "",
+            content: post?.content || "",
+            status: post?.status || "active",
+        },
+    })
+
+    const navigate = useNavigate();
+    const userData = useSelector((state) => state.auth.userData);
+    const submit = async (data) => {
+        if (post) {
+            const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
+
+            if (file) {
+                appwriteService.deleteFile(post.featuredImage);
+            }
+
+            const dbPost = await appwriteService.updatePost(post.$id, {
+                ...data,
+                featuredImage: file ? file.$id : undefined,
+            });
+
+            if (dbPost) {
+                navigate(`/post/${dbPost.$id}`);
+            }
+        } else {
+            const file = await appwriteService.uploadFile(data.image[0]);
+
+            if (file) {
+                const fileId = file.$id;
+                data.featuredImage = fileId;
+                const dbPost = await appwriteService.createPost({ ...data, userId: userData.$id });
+
+                if (dbPost) {
+                    navigate(`/post/${dbPost.$id}`);
+                }
+            }
+        }
+    };
+  return (
+    <div>PostForm</div>
+  )
+}
+
+export default PostForm
